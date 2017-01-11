186334728e1cd62f3e8fe94632d20fae588fd462
Brightcells <brightcells@gmail.com>
2017-01-11 11:42:48 +0800

    :sparkles: Redis Storage support

--- README.md
+++ README.md
@@ -33,11 +33,26 @@ pip install pywe-token
   Out[2]: u'ysR7_hUtodKCF1nHjq8gFtagugB8oEOlK6hB6raMztveawVzpnqK2FtftbQGsczTj0h2kc1Gl8R7fjmGVPmXBp306WW8UZUteXqiOgxh3DL0usLRLQVRn56Oi-yigkSoSYNbAIAEKZ'
   ```
 
+* Redis Storage
+  ```python
+  In [1]: import redis_extensions as redis
+
+  In [2]: r = redis.StrictRedisExtensions(host='localhost', port=6379, db=0)
+
+  In [3]: from pywe_token import access_token
+
+  In [4]: access_token('wx7aad305aed68bfe3', '9eac636765940ec286055c559ff84039', r)
+  Out[4]: u'5kJwbClb1CBo-5Dz_a9hZi1GcqSnLkRV2aYFmjSBTGEvVrH81XhT2eUjunVSJn_ej2uFXLJarjC0dlI78r-HxCWtTNxSPC06ARG_QqE9FoP7VhJNFsPX5z7tsySsCyEgKEZbAIAGAV'
+
+  In [5]: r.get('pywe:wechat:access:token')
+  Out[5]: '5kJwbClb1CBo-5Dz_a9hZi1GcqSnLkRV2aYFmjSBTGEvVrH81XhT2eUjunVSJn_ej2uFXLJarjC0dlI78r-HxCWtTNxSPC06ARG_QqE9FoP7VhJNFsPX5z7tsySsCyEgKEZbAIAGAV'
+  ```
+
 # Method
 
 ```python
 class Token(WechatUtils):
-    def __init__(self, appid=None, secret=None):
+    def __init__(self, appid=None, secret=None, redis_conn=None, redis_key=None):
 
-def access_token(self, appid=None, secret=None):
+def access_token(self, appid=None, secret=None, redis_conn=None, redis_key=None):
 ```
--- pywe_token/token.py
+++ pywe_token/token.py
@@ -7,27 +7,33 @@ from pywe_utils import WechatUtils
 
 
 class Token(WechatUtils):
-    def __init__(self, appid=None, secret=None):
+    def __init__(self, appid=None, secret=None, redis_conn=None, redis_key=None):
         # 获取access token, Refer: https://mp.weixin.qq.com/wiki/14/9f9c82c1af308e3b14ba9b973f99a8ba.html
         super(Token, self).__init__()
         self.WECHAT_ACCESS_TOKEN = self.API_DOMAIN + '/cgi-bin/token?grant_type=client_credential&appid={appid}&secret={secret}'
         self.appid = appid
         self.secret = secret
+        self.redis_conn = redis_conn
+        self.redis_key = redis_key or 'pywe:wechat:access:token'
         self.token = None
         self.expires_at = None
 
-    def __fetch_access_token(self, appid=None, secret=None):
+    def __fetch_access_token(self, appid=None, secret=None, redis_conn=None, redis_key=None):
         result = self.get(self.WECHAT_ACCESS_TOKEN, appid=appid or self.appid, secret=secret or self.secret)
         if 'expires_in' not in result:
             raise WeChatException(result)
         self.token = result.get('access_token')
-        self.expires_at = int(time.time()) + result.get('expires_in')
+        expires_in = result.get('expires_in')
+        self.expires_at = int(time.time()) + expires_in
+        redis_conn = redis_conn or self.redis_conn
+        if redis_conn:
+            redis_conn.setex(redis_key or self.redis_key, expires_in, self.token)
         return self.token
 
-    def access_token(self, appid=None, secret=None):
+    def access_token(self, appid=None, secret=None, redis_conn=None, redis_key=None):
         if self.token and self.expires_at and self.expires_at - int(time.time()) > 60:
             return self.token
-        return self.__fetch_access_token(appid, secret)
+        return self.__fetch_access_token(appid, secret, redis_conn)
 
 
 token = Token()
