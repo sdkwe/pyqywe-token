28aa2ae6fb76e6e81be921fab9b83b6b62f83049
Brightcells <brightcells@gmail.com>
2018-05-16 01:42:31 +0800

    :sparkles: Add new param refresh_left_seconds

--- pywe_token/basetoken.py
+++ pywe_token/basetoken.py
@@ -7,13 +7,14 @@ from pywe_storage import MemoryStorage
 
 
 class BaseToken(BaseWechat):
-    def __init__(self, appid=None, secret=None, token=None, storage=None, token_fetched_func=None):
+    def __init__(self, appid=None, secret=None, token=None, storage=None, token_fetched_func=None, refresh_left_seconds=3600):
         super(BaseToken, self).__init__()
         self.appid = appid
         self.secret = secret
         self.token = token
         self.storage = storage or MemoryStorage()
         self.token_fetched_func = token_fetched_func
+        self.refresh_left_seconds = refresh_left_seconds
 
         if self.token:
             expires_in = 7200
@@ -28,9 +29,10 @@ class BaseToken(BaseWechat):
     def access_info_key(self):
         return '{0}:access:info'.format(self.appid)
 
-    def update_params(self, appid=None, secret=None, token=None, storage=None, token_fetched_func=None):
+    def update_params(self, appid=None, secret=None, token=None, storage=None, token_fetched_func=None, refresh_left_seconds=3600):
         self.appid = appid or self.appid
         self.secret = secret or self.secret
         self.token = token or self.token
         self.storage = storage or self.storage
         self.token_fetched_func = token_fetched_func or self.token_fetched_func
+        self.refresh_left_seconds = refresh_left_seconds or self.refresh_left_seconds
--- pywe_token/token.py
+++ pywe_token/token.py
@@ -8,17 +8,17 @@ from .basetoken import BaseToken
 
 
 class Token(BaseToken):
-    def __init__(self, appid=None, secret=None, storage=None, token_fetched_func=None):
-        super(Token, self).__init__(appid=appid, secret=secret, storage=storage, token_fetched_func=token_fetched_func)
+    def __init__(self, appid=None, secret=None, storage=None, token_fetched_func=None, refresh_left_seconds=3600):
+        super(Token, self).__init__(appid=appid, secret=secret, storage=storage, token_fetched_func=token_fetched_func, refresh_left_seconds=refresh_left_seconds)
         # 获取access token, Refer: https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421140183
         self.WECHAT_ACCESS_TOKEN = self.API_DOMAIN + '/cgi-bin/token?grant_type=client_credential&appid={appid}&secret={secret}'
 
-    def __about_to_expires(self, expires_at):
-        return expires_at and expires_at - int(time.time()) < 60
+    def __about_to_expires(self, expires_at, refresh_left_seconds=3600):
+        return expires_at and expires_at - int(time.time()) < refresh_left_seconds
 
-    def __fetch_access_token(self, appid=None, secret=None, storage=None, token_fetched_func=None):
+    def __fetch_access_token(self, appid=None, secret=None, storage=None, token_fetched_func=None, refresh_left_seconds=3600):
         # Update Params
-        self.update_params(appid=appid, secret=secret, storage=storage, token_fetched_func=token_fetched_func)
+        self.update_params(appid=appid, secret=secret, storage=storage, token_fetched_func=token_fetched_func, refresh_left_seconds=refresh_left_seconds)
         # Access Info Request
         access_info = self.get(self.WECHAT_ACCESS_TOKEN, appid=self.appid, secret=self.secret)
         # Request Error
@@ -34,22 +34,22 @@ class Token(BaseToken):
         # Return Access Token
         return access_info.get('access_token')
 
-    def access_token(self, appid=None, secret=None, storage=None, token_fetched_func=None):
+    def access_token(self, appid=None, secret=None, storage=None, token_fetched_func=None, refresh_left_seconds=3600):
         # Update Params
-        self.update_params(appid=appid, secret=secret, storage=storage, token_fetched_func=token_fetched_func)
+        self.update_params(appid=appid, secret=secret, storage=storage, token_fetched_func=token_fetched_func, refresh_left_seconds=refresh_left_seconds)
         # Fetch access_info
         access_info = self.storage.get(self.access_info_key)
         if access_info:
             access_token = access_info.get('access_token')
-            if access_token and not self.__about_to_expires(access_info.get('expires_at')):
+            if access_token and not self.__about_to_expires(access_info.get('expires_at'), refresh_left_seconds=refresh_left_seconds):
                 return access_token
-        return self.__fetch_access_token(self.appid, self.secret, self.storage, token_fetched_func=self.token_fetched_func)
+        return self.__fetch_access_token(self.appid, self.secret, self.storage, token_fetched_func=self.token_fetched_func, refresh_left_seconds=refresh_left_seconds)
 
-    def refresh_access_token(self, appid=None, secret=None, storage=None, token_fetched_func=None):
-        return self.__fetch_access_token(appid, secret, storage, token_fetched_func=token_fetched_func)
+    def refresh_access_token(self, appid=None, secret=None, storage=None, token_fetched_func=None, refresh_left_seconds=3600):
+        return self.__fetch_access_token(appid, secret, storage, token_fetched_func=token_fetched_func, refresh_left_seconds=refresh_left_seconds)
 
-    def final_access_token(self, cls=None, appid=None, secret=None, token=None, storage=None, token_fetched_func=None):
-        return token or self.access_token(appid or cls.appid, secret or cls.secret, storage=storage or cls.storage, token_fetched_func=token_fetched_func or cls.token_fetched_func)
+    def final_access_token(self, cls=None, appid=None, secret=None, token=None, storage=None, token_fetched_func=None, refresh_left_seconds=3600):
+        return token or self.access_token(appid or cls.appid, secret or cls.secret, storage=storage or cls.storage, token_fetched_func=token_fetched_func or cls.token_fetched_func, refresh_left_seconds=refresh_left_seconds or cls.refresh_left_seconds)
 
 
 token = Token()
